b2bb80ae8adf9495308e40d4d71b03a2bc1abed9
331617870@qq.com <qq331617870>
2021-01-22 10:41:40 +0800

    新增修改/增加最大行数.最大列数查找

--- execlUtils/execlutils/src/main/java/com/wusha/execlutils/ExeclUtils.java
+++ execlUtils/execlutils/src/main/java/com/wusha/execlutils/ExeclUtils.java
@@ -31,10 +31,11 @@ public class ExeclUtils {
 
     /**
      * 创建一个csv文件的工具类
+     *
      * @param inputStream 数据流
      * @throws Exception
      */
-    public static List<String> ReadExcelUtilsCSV(InputStream inputStream)throws Exception{
+    public static List<String> ReadExcelUtilsCSV(InputStream inputStream) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
         List<String> list = new ArrayList<String>();
         String stemp;
@@ -43,13 +44,16 @@ public class ExeclUtils {
         }
         inputStream.close();
         br.close();
-        return  list;
-    } /**
+        return list;
+    }
+
+    /**
      * 创建一个csv文件的工具类
+     *
      * @param filePath 文件路径
      * @throws Exception
      */
-    public static List<String> ReadExcelUtilsCSV(String filePath)throws Exception{
+    public static List<String> ReadExcelUtilsCSV(String filePath) throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(filePath));
         List<String> list = new ArrayList<String>();
         String stemp;
@@ -57,67 +61,67 @@ public class ExeclUtils {
             list.add(stemp);
         }
         br.close();
-        return  list;
+        return list;
     }
 
     /**
      * XLS数据流格式
+     *
      * @param inputStream
      * @return
      * @throws Exception
      */
-    public static String ReadExcelUtilsXLS (InputStream inputStream)throws Exception {
+    public static String ReadExcelUtilsXLS(InputStream inputStream) throws Exception {
         HSSFWorkbook hssfWorkbook = new HSSFWorkbook(inputStream);
         HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(0);
-//            XSSFWorkbook workbook = new XSSFWorkbook(stream);
-//            XSSFSheet sheet = workbook.getSheetAt(0);
         int rowsCount = hssfSheet.getPhysicalNumberOfRows();
         FormulaEvaluator formulaEvaluator = hssfWorkbook.getCreationHelper().createFormulaEvaluator();
-        String cellInfo="";
+        String cellInfo = "";
         for (int r = 0; r < rowsCount; r++) {
             Row row = hssfSheet.getRow(r);
             int cellsCount = row.getPhysicalNumberOfCells();
             for (int c = 0; c < cellsCount; c++) {
                 String value = getCellAsString(row, c, formulaEvaluator);
-                if (cellInfo.equals("")){
-                    cellInfo=value;
-                }else {
-                    cellInfo = cellInfo +","+ value;
+                if (cellInfo.equals("")) {
+                    cellInfo = value;
+                } else {
+                    cellInfo = cellInfo + "," + value;
                 }
 
             }
         }
         return cellInfo;
     }
+
     /**
      * XLS文件名格式
+     *
      * @param fileName 文件名
      * @return
      * @throws Exception
      */
-    public static String ReadExcelUtils (String fileName)throws Exception {
+    public static String ReadExcelUtils(String fileName) throws Exception {
         InputStream inputStream = new FileInputStream(fileName);
-        Workbook workbook=null;
+        Workbook workbook = null;
         if (fileName.endsWith(".xls")) {
             workbook = new HSSFWorkbook(inputStream);
         } else if (fileName.endsWith(".xlsx")) {
             workbook = new XSSFWorkbook(inputStream);
         }
+        inputStream.close();
         Sheet sheet = workbook.getSheetAt(0);
-        //            XSSFWorkbook workbook = new XSSFWorkbook(stream);
-//            XSSFSheet sheet = workbook.getSheetAt(0);
         int rowsCount = sheet.getPhysicalNumberOfRows();
         FormulaEvaluator formulaEvaluator = workbook.getCreationHelper().createFormulaEvaluator();
-        String cellInfo="";
+        String cellInfo = "";
         for (int r = 0; r < rowsCount; r++) {
             Row row = sheet.getRow(r);
             int cellsCount = row.getPhysicalNumberOfCells();
             for (int c = 0; c < cellsCount; c++) {
                 String value = getCellAsString(row, c, formulaEvaluator);
-                if (cellInfo.equals("")){
-                    cellInfo=value;
-                }else {
-                    cellInfo = cellInfo +","+ value;
+                if (cellInfo.equals("")) {
+                    cellInfo = value;
+                } else {
+                    cellInfo = cellInfo + "," + value;
                 }
 
             }
@@ -128,25 +132,26 @@ public class ExeclUtils {
 
     /**
      * XLS数据流格式
+     *
      * @param inputStream
      * @return
      * @throws Exception
      */
-    public static String ReadExcelUtilsXLSX (InputStream inputStream)throws Exception {
+    public static String ReadExcelUtilsXLSX(InputStream inputStream) throws Exception {
         XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
         XSSFSheet sheet = workbook.getSheetAt(0);
         int rowsCount = sheet.getPhysicalNumberOfRows();
         FormulaEvaluator formulaEvaluator = workbook.getCreationHelper().createFormulaEvaluator();
-        String cellInfo="";
+        String cellInfo = "";
         for (int r = 0; r < rowsCount; r++) {
             Row row = sheet.getRow(r);
             int cellsCount = row.getPhysicalNumberOfCells();
             for (int c = 0; c < cellsCount; c++) {
                 String value = getCellAsString(row, c, formulaEvaluator);
-                if (cellInfo.equals("")){
-                    cellInfo=value;
-                }else {
-                    cellInfo = cellInfo +","+ value;
+                if (cellInfo.equals("")) {
+                    cellInfo = value;
+                } else {
+                    cellInfo = cellInfo + "," + value;
                 }
 
 
@@ -154,6 +159,7 @@ public class ExeclUtils {
         }
         return cellInfo;
     }
+
     protected static String getCellAsString(Row row, int c, FormulaEvaluator formulaEvaluator) {
         String value = "";
         try {
@@ -161,21 +167,21 @@ public class ExeclUtils {
             CellValue cellValue = formulaEvaluator.evaluate(cell);
             switch (cellValue.getCellType()) {
                 case Cell.CELL_TYPE_BOOLEAN:
-                    value = ""+cellValue.getBooleanValue();
+                    value = "" + cellValue.getBooleanValue();
                     break;
                 case Cell.CELL_TYPE_NUMERIC:
                     double numericValue = cellValue.getNumberValue();
-                    if(HSSFDateUtil.isCellDateFormatted(cell)) {
+                    if (HSSFDateUtil.isCellDateFormatted(cell)) {
                         double date = cellValue.getNumberValue();
                         SimpleDateFormat formatter =
                                 new SimpleDateFormat("dd/MM/yy");
                         value = formatter.format(HSSFDateUtil.getJavaDate(date));
                     } else {
-                        value = ""+numericValue;
+                        value = "" + numericValue;
                     }
                     break;
                 case Cell.CELL_TYPE_STRING:
-                    value = ""+cellValue.getStringValue();
+                    value = "" + cellValue.getStringValue();
                     break;
                 default:
             }
@@ -190,6 +196,7 @@ public class ExeclUtils {
      * 此方法为删除execl表格中的一行
      * 开始行数从1开始.
      * 结束行数建议写最大行数,不然中间会出现空缺.(有时间在更改)
+     *
      * @param fileName 文件地址
      * @param startRow 开始的行数
      * @param endRow   结束的行数
@@ -205,12 +212,12 @@ public class ExeclUtils {
         } else if (fileName.endsWith(".xlsx")) {
             workbook = new XSSFWorkbook(inputStream);
         }
+        inputStream.close();
         Sheet sheet = workbook.getSheetAt(0);
-
-        int rowsCount = sheet.getPhysicalNumberOfRows();
-        if (sheet == null) {
+         if (sheet == null) {
             return;
         }
+         //开始从哪一行删除.然后哪一行结束.如果endrow不为list最大值则Excel中会出现空白整行
         sheet.shiftRows(startRow, endRow, -1);
 
         FileOutputStream os = new FileOutputStream(fileName);
@@ -225,20 +232,22 @@ public class ExeclUtils {
 
     /**
      * 获取最大行
+     *
      * @param fileName
      * @return
      * @throws Exception
      */
-    public static   int getMaxRownum(String fileName) throws Exception {
+    public static int getMaxRownum(String fileName) throws Exception {
         InputStream inputStream = new FileInputStream(fileName);
-        Workbook workbook=null;
+        Workbook workbook = null;
         if (fileName.endsWith(".xls")) {
             workbook = new HSSFWorkbook(inputStream);
         } else if (fileName.endsWith(".xlsx")) {
             workbook = new XSSFWorkbook(inputStream);
         }
+        inputStream.close();
         Sheet sheet = workbook.getSheetAt(0);
-
+        //获取最大行
         int rowsCount = sheet.getPhysicalNumberOfRows();
 
         return rowsCount;
@@ -246,25 +255,61 @@ public class ExeclUtils {
 
     /**
      * 获取最大列
+     *
      * @param fileName
      * @return
      * @throws Exception
      */
-    public static   int getMaxColnum(String fileName) throws Exception {
+    public static int getMaxColnum(String fileName) throws Exception {
         InputStream inputStream = new FileInputStream(fileName);
-        Workbook workbook=null;
+        Workbook workbook = null;
         if (fileName.endsWith(".xls")) {
             workbook = new HSSFWorkbook(inputStream);
         } else if (fileName.endsWith(".xlsx")) {
             workbook = new XSSFWorkbook(inputStream);
         }
+        //关闭流
+        inputStream.close();
         Sheet sheet = workbook.getSheetAt(0);
-         Row   row = sheet.getRow(0);
+        Row row = sheet.getRow(0);
+        //获取最大的列
         int colnum = row.getPhysicalNumberOfCells();
         return colnum;
     }
 
+    /**
+     * 修改当前excle整行的整列数据
+     *
+     * @param fileName  文件名字
+     * @param rownum    多少行
+     * @param valueDate 这个行中列的值.
+     * @throws IOException
+     */
+    public static void upWorkbook(String fileName, int rownum, List<String> valueDate) throws IOException {
 
-
+        InputStream inputStream = null;
+        inputStream = new FileInputStream(fileName);
+        Workbook workbook = null;
+        if (fileName.endsWith(".xls")) {
+            workbook = new HSSFWorkbook(inputStream);
+        } else if (fileName.endsWith(".xlsx")) {
+            workbook = new XSSFWorkbook(inputStream);
+        }
+        inputStream.close();
+        //第一个sheet表
+        Sheet sheet = workbook.getSheetAt(0);
+        //如果已有行、列直接获取
+        Row row = sheet.getRow(rownum);//得到行
+        //循环出多少有多少列.把值赋进去
+        for (int i = 0; i < row.getPhysicalNumberOfCells(); i++) {
+            Cell cell = row.getCell(i);//得到列
+            cell.setCellValue(valueDate.get(i));//写数据
+        }
+        FileOutputStream excelFileOutPutStream = new FileOutputStream(fileName);//写数据到这个路径上
+        workbook.write(excelFileOutPutStream);
+        //关闭流.应该放在finl中
+        excelFileOutPutStream.flush();
+        excelFileOutPutStream.close();
+    }
 
 }
